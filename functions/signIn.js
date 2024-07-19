eda362fa3fd8ba93ae125e2faa18dfa98e9009d5
Vaishnavi Yadav <vaishna303@gmail.com>
2024-07-20 02:08:07 +0530

    1.0

--- functions/signIn.js
+++
@@ -1,8 +0,0 @@
-// functions/signIn.js
-exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Sign In successful" })
-    };
-  };
-  
